8414d7b03d37589d0f34cb0f111ede7fcc00485a
Tony Petruzzi <tpetruzzi@gmail.com>
2010-11-20 17:33:33 -0500

    more updates
      * added date formatting dependant on jqueryui
    
      * formatted plugin code into jquery standards
    
      * remove unneeded css declaration
    
      * cleaned up html

--- index.js
+++ index.js
@@ -10,105 +10,127 @@ http://blog.reindel.com, http://www.micc.unifi.it/ferracani
 License:
 Unrestricted. This script is free for both personal and commercial use.
 *****************************************************************************/
-jQuery.fn.accessNews = function( settings ){
-	settings = jQuery.extend({
-		headline : "Top Stories"
-		,speed : "normal"
-		,slideBy : 2
-	}, settings);
-	
-	return this.each(function(){
-		jQuery.fn.accessNews.run( jQuery( this ), settings );
-	});
-};
+(function( $ ){
 
-jQuery.fn.accessNews.run = function( $this, settings )
-{
-	var ul = jQuery( "ul:eq(0)", $this );
-	var li = ul.children();
-	var $next = jQuery( ".next > a", $this );
-	var $back = jQuery( ".back > a", $this );
+	$.fn.accessNews = function(settings){
+		
+		var defaults = {
+			// title for the display
+			headline: "TODAY NEWS:"
+			// ?
+			,speed: "normal"
+			// number of slides to advance when pagnating
+			,slideBy: 2
+			// a valid jquery ui date format
+			,dateFormat: "MM dd yy"
+		};
+		
+		settings = $.extend(defaults, settings);		
+		
+		return this.each(function(){
+			var $this = $(this);
+			methods.run( $this , settings );
+		});
+		
+	};
 	
-	if ( li.length > settings.slideBy )
-	{
-		var liWidth = jQuery( li[0] ).width();
-		var animating = false;
-		ul.css( "width", ( li.length * liWidth ) );
-		
-		$next.click(function()
-		{
-			if ( !animating )
-			{
-				animating = true;
-				offsetLeft = parseInt( ul.css( "left" ) ) - ( liWidth * settings.slideBy );
-				if ( offsetLeft + ul.width() > 0 ){
-					$back.show();
-					ul.animate({
-						left: offsetLeft
-					}, settings.speed, function(){
-						if ( parseInt( ul.css( "left" ) ) + ul.width() <= liWidth * settings.slideBy ) {}
-						animating = false;
-					});
-				}
-				else
-				{
-					animating = false;
-				}
-			}
-			return false;
-		});
-		
-		$back.click(function()
-		{
-			if ( !animating )
-			{
-				animating = true;
-				offsetRight = parseInt( ul.css( "left" ) ) + ( liWidth * settings.slideBy );
-				if ( offsetRight + ul.width() <= ul.width() )
-				{
-					$next.show();
-					ul.animate({
-						left: offsetRight
-					}, settings.speed, function()
-					{
-						if ( parseInt( ul.css( "left" ) ) == 0 ) {}
-						animating = false;
-					});
-				}
-				else
-				{
-					animating = false;
-				}
-			}
-			return false;
-		});
+	var methods = {};
+	
+	methods.formatDate = function( format ){
+		return $.datepicker.formatDate( format, new Date() );
 	}
 	
-	$next.show();
-	jQuery(".description").hide();
-	jQuery('#list').after( [ "<div class=\"view_all\"><span class=\"count\">", settings.headline, " - ", li.length, " total</span></div>" ].join( "" ) );
-	jQuery( ".back").appendTo('.view_all');
-	jQuery( ".next").appendTo('.view_all');
-	var date = jQuery('<div></div>');
-	var firstimg = jQuery( "ul li:eq(0) img", $this );
-	date.appendTo("#container").html("<p class='date'><strong>TODAY NEWS</strong>: 2009 december 28</p>");
-	img = jQuery('<img></img>')
-	img.appendTo("#container");
-	img.attr('src', firstimg.attr('src'));
-	para = jQuery('<div></div>');
-	para.appendTo("#container");
-	var firstli = jQuery( "#newsslider ul li:eq(0)");
-	para.html("<h1>" +  jQuery('a.title', firstli ).text() + "</h1>" + "<p id='paraText'>" + jQuery('p.description', firstli).html() + "</p>");
-	firstli.addClass('selected');
-	li.hover(function ()
-	{
-		li.removeClass('selected');
-		var current = jQuery(this);
-		current.addClass('selected');
-		img.attr('src', current.find('img').attr('src'));
-		para.html("<h1>" +  jQuery('.title', current).text() + "</h1>" + "<p id='paraText'>" + jQuery('.description', current).html() + "</p>");
-	}, function ()
-	{
-		current.parent().css('backgroundColor', 'transparent');
-	});
-};
+	methods.run = function( $this, settings){
+		
+		var ul = $( "ul:eq(0)", $this );
+		var li = ul.children();
+		var $next = $( ".next > a", $this );
+		var $back = $( ".back > a", $this );
+		
+		if ( li.length > settings.slideBy )
+		{
+			var liWidth = $( li[0] ).width();
+			var animating = false;
+			ul.css( "width", ( li.length * liWidth ) );
+			
+			$next.click(function()
+			{
+				if ( !animating )
+				{
+					animating = true;
+					offsetLeft = parseInt( ul.css( "left" ) ) - ( liWidth * settings.slideBy );
+					if ( offsetLeft + ul.width() > 0 ){
+						$back.show();
+						ul.animate({
+							left: offsetLeft
+						}, settings.speed, function(){
+							if ( parseInt( ul.css( "left" ) ) + ul.width() <= liWidth * settings.slideBy ) {}
+							animating = false;
+						});
+					}
+					else
+					{
+						animating = false;
+					}
+				}
+				return false;
+			});
+			
+			$back.click(function()
+			{
+				if ( !animating )
+				{
+					animating = true;
+					offsetRight = parseInt( ul.css( "left" ) ) + ( liWidth * settings.slideBy );
+					if ( offsetRight + ul.width() <= ul.width() )
+					{
+						$next.show();
+						ul.animate({
+							left: offsetRight
+						}, settings.speed, function()
+						{
+							if ( parseInt( ul.css( "left" ) ) == 0 ) {}
+							animating = false;
+						});
+					}
+					else
+					{
+						animating = false;
+					}
+				}
+				return false;
+			});
+		}
+		
+		$next.show();
+		$(".description").hide();
+		$('ul', $this).after( [ "<div class=\"view_all\"><span class=\"count\">", settings.headline, " - ", li.length, " total</span></div>" ].join( "" ) );
+		$( ".back").appendTo('.view_all');
+		$( ".next").appendTo('.view_all');
+		var date = $('<div></div>');
+		var firstimg = $( "ul li:eq(0) img", $this );
+		date.appendTo("#container").html(["<p class='date'><strong>", settings.headline, "</strong> ", methods.formatDate(settings.dateFormat), "</p>"].join(""));
+		img = $('<img></img>')
+		img.appendTo("#container");
+		img.attr('src', firstimg.attr('src'));
+		para = $('<div></div>');
+		para.appendTo("#container");
+		var firstli = $( "ul li:eq(0)", $this);
+		para.html("<h1>" +  $('a.title', firstli ).text() + "</h1>" + "<p id='paraText'>" + $('p.description', firstli).html() + "</p>");
+		firstli.addClass('selected');
+		li.hover(function ()
+		{
+			li.removeClass('selected');
+			var current = $(this);
+			current.addClass('selected');
+			img.attr('src', current.find('img').attr('src'));
+			para.html("<h1>" +  $('.title', current).text() + "</h1>" + "<p id='paraText'>" + $('.description', current).html() + "</p>");
+		}, function ()
+		{
+			current.parent().css('backgroundColor', 'transparent');
+		});
+		
+	};
+	
+	
+})( jQuery );
