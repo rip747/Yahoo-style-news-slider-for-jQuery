c8ec29b1d65aff97b5c835d3e560a6adaa4d34f9
Tony Petruzzi <tpetruzzi@gmail.com>
2010-11-22 18:15:52 -0500

    change id to class

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ Unrestricted. This script is free for both personal and commercial use.
 		li.addClass('selected');		
 		_content.empty();
 		img.attr('src', $('img', li).attr('src'));
-		para.html("<h1>" +  $('a.title', li ).text() + "</h1>" + "<p id='paraText'>" + $('p.description', li).html() + "</p>");
+		para.html("<h1>" +  $('a.title', li ).text() + "</h1>" + "<p class='paraText'>" + $('p.description', li).html() + "</p>");
 		_content.append(img);
 		_content.append(para);
 	}
