e90a4605bf4848bc611a26c90a1c8965231bc07a
Tony Petruzzi <tpetruzzi@gmail.com>
2010-11-22 14:32:10 -0500

    remove outside wrapper dependancy

--- index.js
+++ index.js
@@ -41,12 +41,11 @@ Unrestricted. This script is free for both personal and commercial use.
 	}
 	
 	methods.draw = function ($this, settings){
-		var ul = $( "ul", $this );
-		var li = ul.children();
-		var firstli = $( "li:eq(0)", ul);
+		var li = $this.children();
+		var firstli = $("li:eq(0)", $this);
 		var firstimg = $( "img", firstli );
 		
-		methods.initContainer(ul);
+		methods.initContainer($this);
 
 		var nextBackLinks = "<span class=\"back\"><a href=\"#\" title=\"Back\">&lt;&lt; Back</a></span> <span class=\"next\"><a href=\"#\" title=\"Next\">Next &gt;&gt;</a></span>";
 		var _date = ["<p class='date'><strong>", settings.headline, "</strong> ", methods.formatDate(settings.dateFormat), "</p>"].join("");
@@ -55,28 +54,20 @@ Unrestricted. This script is free for both personal and commercial use.
 		var img = $('<img></img>');
 		var para = $('<div></div>');
 		
-		ul.after( viewAll );
+		$this.after( viewAll );
 		img.attr('src', firstimg.attr('src'));
 		para.html("<h1>" +  $('a.title', firstli ).text() + "</h1>" + "<p id='paraText'>" + $('p.description', firstli).html() + "</p>");
 		
-		methods.setContainer(ul, date);
-		methods.setContainer(ul, img);
-		methods.setContainer(ul, para);
+		methods.setContainer($this, date);
+		methods.setContainer($this, img);
+		methods.setContainer($this, para);
 		
 		firstli.addClass('selected');
 	}
 	
-	/*
-	methods.currentStory = function (context){
-		context.addClass('selected');
-		img.attr('src', current.find('img').attr('src'));
-		para.html("<h1>" +  $('.title', current).text() + "</h1>" + "<p id='paraText'>" + $('.description', current).html() + "</p>");
-	}
-	*/
-	
 	methods.initContainer = function (context){
-		var container = "<div class=\"container\"></div>";
-		context.before( container );
+		context.wrap("<div class=\"accessible_news_slider business_as_usual\"></div>");
+		context.before("<div class=\"container\"></div>");
 	}
 	
 	methods.setContainer = function (context, content){
@@ -91,32 +82,31 @@ Unrestricted. This script is free for both personal and commercial use.
 		
 	methods.run = function( $this, settings){
 		
-		var ul = $( "ul:eq(0)", $this );
-		var li = ul.children();
+		var li = $this.children();
 		
 		methods.draw ($this, settings);
 		
-		var $next = $( ".next > a", $this );
-		var $back = $( ".back > a", $this );
-		
+		var viewAll = $this.next("div.view_all");		
+		var $next = $( ".next > a", viewAll );
+		var $back = $( ".back > a", viewAll );
+
 		if ( li.length > settings.slideBy )
 		{
 			var liWidth = $( li[0] ).width();
 			var animating = false;
-			ul.css( "width", ( li.length * liWidth ) );
-			
+			$this.css( "width", ( li.length * liWidth ) );
 			$next.click(function()
 			{
 				if ( !animating )
 				{
 					animating = true;
-					offsetLeft = parseInt( ul.css( "left" ) ) - ( liWidth * settings.slideBy );
-					if ( offsetLeft + ul.width() > 0 ){
+					offsetLeft = parseInt( $this.css( "left" ) ) - ( liWidth * settings.slideBy );
+					if ( offsetLeft + $this.width() > 0 ){
 						$back.show();
-						ul.animate({
+						$this.animate({
 							left: offsetLeft
 						}, settings.speed, function(){
-							if ( parseInt( ul.css( "left" ) ) + ul.width() <= liWidth * settings.slideBy ) {}
+							if ( parseInt( $this.css( "left" ) ) + $this.width() <= liWidth * settings.slideBy ) {}
 						});
 					}
 					animating = false;
@@ -129,15 +119,15 @@ Unrestricted. This script is free for both personal and commercial use.
 				if ( !animating )
 				{
 					animating = true;
-					offsetRight = parseInt( ul.css( "left" ) ) + ( liWidth * settings.slideBy );
-					if ( offsetRight + ul.width() <= ul.width() )
+					offsetRight = parseInt( $this.css( "left" ) ) + ( liWidth * settings.slideBy );
+					if ( offsetRight + $this.width() <= $this.width() )
 					{
 						$next.show();
-						ul.animate({
+						$this.animate({
 							left: offsetRight
 						}, settings.speed, function()
 						{
-							if ( parseInt( ul.css( "left" ) ) == 0 ) {}
+							if ( parseInt( $this.css( "left" ) ) == 0 ) {}
 						});
 					}
 					animating = false;
