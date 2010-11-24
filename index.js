330b5c4c36a9400c87bb3056828f6222ca555c36
Tony Petruzzi <tpetruzzi@gmail.com>
2010-11-24 17:56:33 -0500

    refactored the code into objects

--- index.js
+++ index.js
@@ -11,164 +11,150 @@ License:
 Unrestricted. This script is free for both personal and commercial use.
 *****************************************************************************/
 (function( $ ){
-
 	$.fn.accessNews = function(settings){
-		
+	
 		var defaults = {
 			// title for the display
-			headline: "TODAY NEWS:"
+			headline: "TODAY NEWS:",
 			// ?
-			,speed: "normal"
+			speed: "normal",
 			// number of slides to advance when pagnating
-			,slideBy: 2
+			slideBy: 4,
 			// a valid jquery ui date format
-			,dateFormat: "MM dd yy"
+			dateFormat: "MM dd yy"
 		};
 		
-		settings = $.extend(defaults, settings);		
-		
-		return this.each(function(){
-			var $this = $(this);
-			methods.init( $this , settings );
-		});
-		
-	};
-	
-	var methods = {};
-	
-	// call jquery-ui to format the date display
-	methods.formatDate = function( format ){
-		return $.datepicker.formatDate( format, new Date() );
-	}
-	
-	// draws and sets up the prev / next links if need be
-	methods.prevNext = function($this, settings){
-		var li = $this.children();
-		
-		// only draw prev and next links if need be
-		if ( li.length <= settings.slideBy )
-		{
-			return;
+		// call jquery-ui to format the date display
+		var formatDate = function(format){
+			return jQuery.datepicker.formatDate(format, new Date());
 		}
-		var animating = false;
-		var viewAll = $this.next("div.view_all");
-		var nextBackLinks = "<div class=\"controls\"><span class=\"back\"><a href=\"#\" title=\"Back\">&lt;&lt; Back</a></span><span class=\"next\"><a href=\"#\" title=\"Next\">Next &gt;&gt;</a></span></div>";
-
-		viewAll.append(nextBackLinks);
+		
+		return this.each(function(settings){
+			settings = jQuery.extend(defaults, settings);
+			var _this = jQuery(this);
+			var lis = _this.children();
+			var first = jQuery(lis[0]);
+			var liWidth = first.width();
+			_this.css("width", (lis.length * liWidth));
+			
+			var container = {
+			
+				_wrapper: "<div class=\"accessible_news_slider business_as_usual\"></div>",
+				_container: "<div class=\"container\"></div>",
+				_date: jQuery("<div class='date'></div>").html(["<p><strong>", settings.headline, "</strong> ", formatDate(settings.dateFormat), "</p>"].join("")),
+				_content: jQuery("<div class='content'></div>"),
+				_first: jQuery("li:eq(0)", _this),
 				
-		var $next = $( ".next > a", viewAll );
-		var $back = $( ".back > a", viewAll );
-		var liWidth = $( li[0] ).width();
-		
-		$this.css( "width", ( li.length * liWidth ) );
-		$next.click(function()
-		{
-			if ( !animating )
-			{
-				animating = true;
-				offsetLeft = parseInt( $this.css( "left" ) ) - ( liWidth * settings.slideBy );
-				if ( offsetLeft + $this.width() > 0 ){
-					$back.show();
-					$this.animate({
-						left: offsetLeft
-					}, settings.speed, function(){
-						if ( parseInt( $this.css( "left" ) ) + $this.width() <= liWidth * settings.slideBy ) {}
-					});
+				init: function(){
+					// wrap the ul with our div class and assigned theme
+					_this.wrap(this._wrapper);
+					// our container where we show the image and news item
+					_this.before(this._container);
+					this.append(this._date);
+					this.append(this._content);
+					this.set(this._first);
+					
+				},
+				
+				append: function(content){
+					this.get().append(content);
+				},
+				
+				// returns the main container
+				get: function(){
+					return _this.prev("div.container");
+				},
+				
+				set: function(li){
+					var container = this.get();
+					var _content = jQuery("div.content", container);
+					var img = jQuery('<img></img>');
+					var para = jQuery('<div></div>');
+					li.addClass('selected');
+					_content.empty();
+					img.attr('src', jQuery('img', li).attr('src'));
+					var title = jQuery('p.title a', li);
+					title = title.attr('title') || title.text();
+					para.html("<h1>" + title + "</h1>" + "<p class='paraText'>" + jQuery('p.description', li).html() + "</p>");
+					_content.append(img);
+					_content.append(para);
 				}
-				animating = false;
-			}
-			return false;
-		});
-		
-		$back.click(function()
-		{
-			if ( !animating )
-			{
-				animating = true;
-				offsetRight = parseInt( $this.css( "left" ) ) + ( liWidth * settings.slideBy );
-				if ( offsetRight + $this.width() <= $this.width() )
-				{
-					$next.show();
-					$this.animate({
-						left: offsetRight
-					}, settings.speed, function()
-					{
-						if ( parseInt( $this.css( "left" ) ) == 0 ) {}
+				
+			};
+			
+			var prevNext = {
+			
+				viewAll: ["<div class=\"view_all\"><div class=\"count\"><span>", lis.length, " total</span></div></div>"].join(""),
+				
+				init: function(){
+					_this.after(this.viewAll);
+					if (lis.length > settings.slideBy) {
+						this.draw();
+					}
+				},
+				
+				draw: function(){
+				
+					var animating = false;
+					var viewAll = _this.next("div.view_all");
+					var nextBackLinks = "<div class=\"controls\"><span class=\"back\"><a href=\"#\" title=\"Back\">&lt;&lt; Back</a></span><span class=\"next\"><a href=\"#\" title=\"Next\">Next &gt;&gt;</a></span></div>";
+					
+					viewAll.append(nextBackLinks);
+					
+					var _next = jQuery(".next > a", viewAll);
+					var _back = jQuery(".back > a", viewAll);
+					var liWidth = jQuery(lis[0]).width();
+					
+					_next.click(function(){
+						if (!animating) {
+							animating = true;
+							offsetLeft = parseInt(_this.css("left")) - (liWidth * settings.slideBy);
+							if (offsetLeft + _this.width() > 0) {
+								_back.show();
+								_this.animate({
+									left: offsetLeft
+								}, settings.speed, function(){
+									if (parseInt(_this.css("left")) + _this.width() <= liWidth * settings.slideBy) {
+									}
+								});
+							}
+							animating = false;
+						}
+						return false;
 					});
+					
+					_back.click(function(){
+						if (!animating) {
+							animating = true;
+							offsetRight = parseInt(_this.css("left")) + (liWidth * settings.slideBy);
+							if (offsetRight + _this.width() <= _this.width()) {
+								_next.show();
+								_this.animate({
+									left: offsetRight
+								}, settings.speed, function(){
+									if (parseInt(_this.css("left")) == 0) {
+									}
+								});
+							}
+							animating = false;
+						}
+						return false;
+					});
+					
 				}
-				animating = false;
-			}
-			return false;
-		});
-		
-	}
-	
-	// sets the content area of the container
-	methods.content = function ($this, li){
-		var container = methods.get($this);
-		var _content = $("div.content", container);
-		var img = $('<img></img>');
-		var para = $('<div></div>');
-		li.addClass('selected');		
-		_content.empty();
-		img.attr('src', $('img', li).attr('src'));
-		var title = $('p.title a', li );
-		title = title.attr('title') || title.text();
-		para.html("<h1>" +  title + "</h1>" + "<p class='paraText'>" + $('p.description', li).html() + "</p>");
-		_content.append(img);
-		_content.append(para);
-	}
-	
-	// sets up the main container to display
-	methods.setup = function (context, settings){
-		var li = context.children();
-		var firstli = $("li:eq(0)", context);
-	
-		context.wrap("<div class=\"accessible_news_slider business_as_usual\"></div>");
-		context.before("<div class=\"container\"></div>");
-	
-		var _date = ["<p><strong>", settings.headline, "</strong> ", methods.formatDate(settings.dateFormat), "</p>"].join("");
-		var date = $("<div class='date'></div>").html(_date);
-		var viewAll = [ "<div class=\"view_all\"><div class=\"count\"><span>", li.length, " total</span></div></div>" ].join( "" );
-		var content = $("<div class='content'></div>");
-		
-		methods.set(context, date);
-		methods.set(context, content);
-		methods.content(context, firstli);
-		
-		context.after( viewAll );
-		
-		methods.prevNext(context, settings);
-	}
-	
-	// append some content to the main container
-	methods.set = function (context, content){
-		var container = methods.get(context);
-		container.append(content);
-	}
-	
-	// returns the main container
-	methods.get = function (context){
-		var container = context.prev("div.container");
-		return container;
-	} 
-	
-	// call to run the plugin
-	methods.init = function( $this, settings){
-		
-		var li = $this.children();
-		
-		methods.setup($this, settings);
+			};
+			
+			//setup the container
+			container.init();
+			// append hover every to each element to update container content
+			prevNext.init();
+			lis.hover(function(){
+				lis.removeClass('selected');
+				container.set(jQuery(this));
+			}, function (){
+				current.parent().css('backgroundColor', 'transparent');
+			})
 
-		li.hover(function ()
-		{
-			li.removeClass('selected');
-			methods.content($this, $(this));
-		}, function ()
-		{
-			current.parent().css('backgroundColor', 'transparent');
 		});
-		
 	};
-	
 })( jQuery );
